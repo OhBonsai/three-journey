733c6fbcb6bdaf6dc99d8eb5fb03afa4ed8c44ef
ohbonsai <letbonsaibe@gmail.com>
2023-06-18 11:07:34 +0800

    just play lesson 4-8

--- 45/src/script.js
+++ 45/src/script.js
@@ -1,53 +1,88 @@
 import * as THREE from 'three'
+import {gsap} from "gsap";
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+
 const size = {
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
 }
 
 const scene = new THREE.Scene()
-const group = new THREE.Group(
-)
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: "red"}),
-)
 
 const cube2 = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: "blue"}),
+    new THREE.MeshBasicMaterial({color: "green"}),
 )
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: "yellow"}),
-)
-
-cube1.position.x = -2
-cube3.position.x = 2;
-
-group.add(cube1)
-group.add(cube2)
-group.add(cube3)
-
-group.scale.y = 2;;
-group.rotation.set(0, Math.PI * 2 , 0 )
 
 const camera = new THREE.PerspectiveCamera(75, size.width/size.height)
 scene.add(camera)
 camera.position.z = 5;
 
-
-
-
-
-
-
+scene.add(cube2)
+var target = document.getElementById("webgl");
 const renderer = new THREE.WebGL1Renderer({
-    canvas: document.getElementById("webgl")
+    canvas: target
+})
+
+const cursor = {
+    x: 0,
+    y: 0,
+}
+
+window.addEventListener("mousemove", (event)=>{
+    cursor.x = event.clientX / size.width - 0.5
+    cursor.y = event.clientY / size.height - 0.5
+})
+
+const control = new OrbitControls(camera, target);
+// control.enableDamping = true;
+// control.enabled = false
+renderer.setSize(size.width, size.height)
+const vars1 ={
+    x: 3,
+    duration: 3,
+    delay: 0.5,
+    // repeat: 1,
+    ease: "power2.inOut",
+}
+// gsap.to(cube2.position, vars1)
+
+function tick() {
+//
+//     camera.position.x = Math.sin(cursor.x) * 5;
+//     camera.position.z = Math.cos(cursor.x)
+//     camera.position.y = - cursor.y * 5
+//     camera.lookAt(cube2.position)
+    requestAnimationFrame(tick)
+    control.update()
+renderer.render(scene, camera)
+}
+
+
+window.addEventListener('resize', ()=>{
+    console.log('window hase been resized')
+    size.width = window.innerWidth
+    size.height = window.innerHeight
+
+    renderer.setSize(size.width, size.height)
+    camera.aspect = size.width / size.height
+    camera.updateProjectionMatrix()
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 })
 
 
-renderer.setSize(size.width, size.height)
-renderer.render(scene, camera)
+window.addEventListener('dblclick', ()=>{
+    if (!document.fullscreenElement) {
+        target.requestFullscreen()
+        console.log('go')
+    } else {
+        console.log('leave')
+        document.exitFullscreen()
+    }
+})
+
+tick()
+
+
+
